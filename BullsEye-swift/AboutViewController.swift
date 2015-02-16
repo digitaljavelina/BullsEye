197dc70e399409ff2252390b54115feb2e5eefe8
Michael Henry <mike@digitaljavelina.com>
2015-02-16 16:26:18 -0700

    auto layout and launch images added

--- BullsEye-swift/AboutViewController.swift
+++ BullsEye-swift/AboutViewController.swift
@@ -17,13 +17,19 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var webView: UIWebView!
     
 
     /*
--- BullsEye-swift/ViewController.swift
+++ BullsEye-swift/ViewController.swift
@@ -19,6 +19,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         startNewGame()
         updateLabels()
+        
+        //Slider UI code
+        
+        let thumbnailImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbnailImageNormal, forState: .Normal)
+        
+        let thumbnailImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbnailImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,10 +53,10 @@ class ViewController: UIViewController {
         
         var title: String
         if difference == 0 {
-            title = "Perfect! You get 100 Bonus Points!"
+            title = "Perfect! \nYou get 100 Bonus Points!"
             score += 100
         } else if difference < 2 {
-            title = "So close! You get 50 Bonus Points!"
+            title = "So close! \nYou get 50 Bonus Points!"
             score += 50
         } else if difference < 5 {
             title = "You almost had it!"
